116b2c2b8f8a0f86f98a0cfa07c58bf62a09a2d1
Henry Miguel Guzmán Escorcia <hguzman10@gmail.com>
2016-07-12 13:41:16 -0500

    Se implementa el inicio de session para el proceso

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '4.2.6'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
 # Use SCSS for stylesheets
+
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
@@ -35,6 +37,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'rspec-rails', '~> 3.5', '>= 3.5.1'  
 end
 
 group :development do
@@ -44,4 +47,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
+
+gem 'devise', '~> 4.2'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  ActionMailer::Base.smtp_settings = {  
+    :address              => "smtp.gmail.com",  
+    :port                 => 587,  
+    :domain               => "gmail.com",  
+    :user_name            => "hmguzman1@misena.edu.co",  
+    :password             => "usrio01",  
+    :authentication       => "plain"
+    # :enable_starttls_auto => true # I don't have this, but it should work anyway 
+  } 
 end
